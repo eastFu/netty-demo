d6a496e4f59bf4277b2d43599e16ecae13a91eee
eastFu <492510931@qq.com>
2018-08-27 19:27:12 +0800

    netty time server

---
+++ netty-demo/src/main/java/per/east/basic/aio/AsyncTimeClientHandler.java
@@ -0,0 +1,110 @@
+package per.east.basic.aio;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.CountDownLatch;
+
+public class AsyncTimeClientHandler implements Runnable,CompletionHandler<Void,AsyncTimeClientHandler>{
+
+    private AsynchronousSocketChannel client;
+
+    private String host;
+
+    private int port;
+
+    private CountDownLatch latch;
+
+    public AsyncTimeClientHandler(String host,int port){
+        this.host = host;
+        this.port = port;
+        try {
+            client = AsynchronousSocketChannel.open();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        latch = new CountDownLatch(1);
+        client.connect(new InetSocketAddress(this.host,this.port),this,this);
+        try {
+            latch.await();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try {
+            client.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void completed(Void result, AsyncTimeClientHandler attachment) {
+        byte[] req = "QUERY TIME ORDER".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+        writeBuffer.put(req);
+        writeBuffer.flip();
+        client.write(writeBuffer, writeBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+            @Override
+            public void completed(Integer result, ByteBuffer attachment) {
+                if(attachment.hasRemaining()){
+                    client.write(attachment,attachment,this);
+                }else{
+                    ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                    client.read(readBuffer, readBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+                        @Override
+                        public void completed(Integer result, ByteBuffer attachment) {
+                            attachment.flip();
+                            byte[] bytes = new byte[attachment.remaining()];
+                            attachment.get(bytes);
+                            String body ;
+                            try {
+                                body = new String(bytes,"UTF-8");
+                                System.out.println("Now is : "+body);
+                                latch.countDown();
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void failed(Throwable exc, ByteBuffer attachment) {
+                            try {
+                                client.close();
+                                latch.countDown();
+                            }catch (Exception e){
+                                //ignore on close
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, ByteBuffer attachment) {
+                try {
+                    client.close();
+                    latch.countDown();
+                }catch (Exception e){
+                    //ignore on close
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void failed(Throwable exc, AsyncTimeClientHandler attachment) {
+        try {
+            client.close();
+            latch.countDown();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ netty-demo/src/main/java/per/east/basic/bio/TimeClient.java
@@ -0,0 +1,63 @@
+package per.east.basic.bio;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @ClassName: TimeClient
+ * @Description: 同步阻塞式IO创建的TimeClient
+ * @author East.F
+ * @date 2017年3月25日 下午11:27:38
+ */
+public class TimeClient {
+
+	public static void main(String[] args) {
+		int port=8080;
+		if(args!=null&&args.length>0){
+			try {
+				port=Integer.parseInt(args[0]);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		Socket socket=null;
+		BufferedReader in =null;
+		PrintWriter out=null;
+		try {
+			socket=new Socket("127.0.0.1",port);
+			in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out=new PrintWriter(socket.getOutputStream(),true);
+			out.println("QUERY TIME ORDER");
+			System.out.println("Send order 2 server succeed.");
+			String resp=in.readLine();
+			System.out.println("Now is :" +resp);
+		} catch (Exception e) {
+			//不处理
+		}finally {
+			if(out!=null){
+				out.close();
+				out=null;
+			}
+			if(in!=null){
+				try {
+					in.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+				in=null;
+			}
+			if(socket!=null){
+				try {
+					socket.close();
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+				socket=null;
+			}
+			
+		}
+		
+	}
+}
